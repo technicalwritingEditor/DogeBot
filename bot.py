09f04e7ddf935300ea3f71442f8da74512b556e6
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-09 21:19:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ bot.load_extension("cogs.fun")
 bot.load_extension("cogs.images")
 bot.load_extension("cogs.info")
 bot.load_extension("cogs.utility")
+bot.load_extension("cogs.pokemon")
 
 @bot.event
 async def on_ready():
