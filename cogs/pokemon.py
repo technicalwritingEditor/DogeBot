29c6187f9a1b2e758d8647ef1585d354a5ad67bb
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-09 21:18:31 +0200

    Adding pokemon back

---
+++ cogs/pokemon.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import random, asyncio, aiohttp, ezjson
+
+class pokemon():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def pokemon(self, ctx):
+        num = random.randint(1, 926)
+        async with aiohttp.ClientSession().get(f'https://pokeapi.co/api/v2/pokemon-form/{num}/') as resp:
+            data = await resp.json()
+        embed = discord.Embed(title='Who\'s that pokemon?')
+        embed.set_image(url=data['sprites']['front_default'])
+        await ctx.send(embed=embed)
+        await self.bot.db.configs.update_one({ "id": ctx.author.id }, { "$set": { "pokemon": data['id'] } }, upsert=True)
+
+    @commands.command()
+    async def mypokemon(self, ctx):
+        user = await self.bot.db.configs.find_one({ "id": ctx.author.id })
+        await ctx.send(user['pokemon'])
+
+
+def setup(bot):
+    bot.add_cog(pokemon(bot))
