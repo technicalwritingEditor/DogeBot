0e40e94bb234ae01319f85193a91c6e15ae4c2c2
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-09 18:53:36 +0200

    so

--- cogs/fun.py
+++ cogs/fun.py
@@ -54,7 +54,7 @@ class fun():
 	    num = random.randint(1, 926)
 	    async with aiohttp.ClientSession().get(f'https://pokeapi.co/api/v2/pokemon-form/{num}/') as resp:
 		    data = await resp.json()
-	    await ctx.send(f"You caught a {data['name']}" + data['sprites']['front_default'])
+	    await ctx.send(f"You caught a {data['name']}\n{data['sprites']['front_default']}")
 	    await self.bot.db.configs.update_one({ "id": ctx.author.id }, { "$set": { "pokemon": data['sprites']['front_default'] } }, upsert=True)
 		
     @commands.command()
