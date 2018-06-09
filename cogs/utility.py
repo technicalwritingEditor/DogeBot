936623efc13651cda3eec5ab438024db9ac66a42
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-09 21:11:01 +0200

    so

--- cogs/utility.py
+++ cogs/utility.py
@@ -24,22 +24,6 @@ class utility():
     async def userinfo(self, ctx, user: discord.Member):
         embed=discord.Embed(description=f"{user.mention}\nId: **{user.id}**\nRoles: **{len(user.roles)}**\nStatus: **{user.status}**\nJoined at: **{user.joined_at}**", color=user.color)
         await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.has_permissions(manage_guild = True)
-    async def prefix(self, ctx, prefix=None):
-        em = discord.Embed(color=discord.Color(value=0x00ff00), title="Bot Prefix")
-        if prefix is None:
-            em.description = f"The bot's prefix for server **{ctx.guild.name}** is set to `{ctx.prefix}`."
-            return await ctx.send(embed=em)
-        if prefix.lower() == 'clear':
-            await self.bot.db.prefix.update_one({"id": str(ctx.guild.id)}, {"$set": {"prefix": "*"}}, upsert=True)
-            em.description = f"The bot's prefix is now set to the default: `*`."
-            return await ctx.send(embed=em)
-        else:
-            await self.bot.db.prefix.update_one({"id": str(ctx.guild.id)}, {"$set": {"prefix": prefix}}, upsert=True)
-            em.description = f"The bot's prefix for this server is set to: `{prefix}`."
-            return await ctx.send(embed=em)
         
 def setup(bot):
     bot.add_cog(utility(bot))
