ba193799dc8a20399b4c13e50dbc4ec4e7f177f1
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-09 21:25:14 +0200

    pls

--- bot.py
+++ bot.py
@@ -53,6 +53,7 @@ async def help(ctx):
     embed=discord.Embed(title="My commands", color=0x9b9dff)
     embed.add_field(name="Info", value="`help` `info` `invite`", inline=False)
     embed.add_field(name="Fun", value="`roast` `face` `lenny` `tableflip`", inline=False)
+    embed.add_field(name="Pokemon", value="`pokemon` `mypokemon`", inline=False)
     embed.add_field(name="Images", value="`rip` `achievement` `avatar`")
     embed.add_field(name="Utility", value="`8ball` `serverinfo` `userinfo`", inline=False)
     embed.set_footer(text="I´m a very new bot and in early development, there will come A LOT more commands!")
