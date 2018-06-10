7fdb9108f85f6cd789a1ab0beb7a544721091104
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-10 11:28:37 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -16,6 +16,16 @@ class mod():
             return
         await send_channel.send(f"Welcome {user.mention} to the guild!")
 
+    async def on_member_remove(self, user):
+        x = await self.bot.db.leave.find_one({"id": str(user.guild.id)})
+        if not x:
+            return
+        channel = int(x['channel'])
+        send_channel = self.bot.get_channel(channel)
+        if not send_channel:
+            return
+        await send_channel.send(f"Goodbye **{user.name}**")        
+        
     @commands.command()
     async def welcome(self, ctx):
         await ctx.send("Please mention the channel to set welcome messages in.")
@@ -32,6 +42,24 @@ class mod():
             return await ctx.send("Did you properly mention a channel? Probably not.")
         await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel} }, upsert=True )
         await ctx.send("I have set the welcome channel!")
-
+        
+    @commands.command()
+    async def leave(self, ctx):
+        await ctx.send("Please mention the channel to set leave messages in.")
+        try:
+            x = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
+        except asyncio.TimeoutError:
+            return await ctx.send("The time is up")
+        if not x.content.startswith("<#") and not x.content.endswith(">"):
+            return await ctx.send("Please mention the channel")
+        channel = x.content.strip("<#").strip(">")
+        try:
+            channel = int(channel)
+        except ValueError:
+            return await ctx.send("Please mention the channel right")
+        await self.bot.db.leave.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel} }, upsert=True )
+        await ctx.send("I have set the leave channel!")
+        
+        
 def setup(bot):
     bot.add_cog(mod(bot))
