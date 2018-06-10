54c21886309aa2733e195e798fe78e5a60d7b866
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-10 14:13:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def on_ready():
 async def help(ctx):
     embed=discord.Embed(title="My commands", color=0x9b9dff)
     embed.add_field(name="Info", value="`help` `info` `invite`", inline=False)
-    embed.add_field(name="Fun", value="`roast` `face` `lenny` `tableflip`", inline=False)
+    embed.add_field(name="Fun", value="`roast` `face` `lenny` `tableflip` `dog`", inline=False)
     embed.add_field(name="Economy", value="`create` `work` `bal` `daily`", inline=False)
     embed.add_field(name="Images", value="`rip` `achievement` `avatar`", inline=False)
     embed.add_field(name="Moderation", value="`welcome` `leave`", inline=False)
