9e5e93d6678195a3addf79b1788df708bfb686b9
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-10 19:14:14 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -106,21 +106,27 @@ class mod():
             await ctx.send("**I have turned off leave messages**")             
 
     @commands.command()
-    async def modlog(self, ctx):
-        await ctx.send("Please mention the channel to set the log messages in.")
-        try:
-            x = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
-        except asyncio.TimeoutError:
-            return await ctx.send("The time is up")
-        if not x.content.startswith("<#") and not x.content.endswith(">"):
-            return await ctx.send("Please mention the channel")
-        channel = x.content.strip("<#").strip(">")
-        try:
-            channel = int(channel)
-        except ValueError:
-            return await ctx.send("Please mention the channel right")
-        await self.bot.db.modlog.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel} }, upsert=True )
-        await ctx.send("I have set the mod-log channel!")           
+    async def modlog(self, ctx, sort=None):
+        if sort == None:
+            await ctx.send("**`on` or `off`**")
+        if sort == "on":
+            await ctx.send("**Please mention the channel to set the log messages in.**")
+            try:
+                x = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
+            except asyncio.TimeoutError:
+                return await ctx.send("**The time is up**")
+            if not x.content.startswith("<#") and not x.content.endswith(">"):
+                return await ctx.send("**Please mention the channel**")
+            channel = x.content.strip("<#").strip(">")
+            try:
+                channel = int(channel)
+            except ValueError:
+                return await ctx.send("**Please mention the channel right**")
+            await self.bot.db.modlog.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel} }, upsert=True )
+            await ctx.send("**I have set the mod-log channel!**")
+        if sort == "off":
+            await self.bot.db.modlog.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": False} }, upsert=True )
+            await ctx.send("**I have turned off modlog messages**")       
             
             
 def setup(bot):
