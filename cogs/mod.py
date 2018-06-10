bba77ca8d557c93552604fff874921bd0b0b4b9b
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-10 11:50:19 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -30,6 +30,42 @@ class mod():
             return
         await send_channel.send(embed=em)   
         
+    async def on_reaction_add(self, reaction, user):
+        if reaction.emoji == "⭐":
+            embed=discord.Embed(title="Starboard", description=reaction.message.content, color=0xffff80)
+            embed.set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url)
+            try:
+                img_url = reaction.message.attachments[0].url
+                if img_url:
+                    embed.set_image(url=str(img_url))
+            except IndexError:
+                img_url = None
+        x = await self.bot.db.startboard.find_one({"id": str(user.guild.id)})
+        if not x:
+            return
+        channel = int(x['channel'])
+        send_channel = self.bot.get_channel(channel)
+        if not send_channel:
+            return
+        await send_channel.send(embed=embed)
+
+    @commands.command()
+    async def starboard(self, ctx):
+        await ctx.send("Please mention the channel to set the starboard messages in.")
+        try:
+            x = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
+        except asyncio.TimeoutError:
+            return await ctx.send("The time is up")
+        if not x.content.startswith("<#") and not x.content.endswith(">"):
+            return await ctx.send("Please mention the channel")
+        channel = x.content.strip("<#").strip(">")
+        try:
+            channel = int(channel)
+        except ValueError:
+            return await ctx.send("Please mention the channel right")
+        await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel} }, upsert=True )
+        await ctx.send("I have set the starboard channel!")        
+        
     @commands.command()
     async def welcome(self, ctx):
         await ctx.send("Please mention the channel to set welcome messages in.")
