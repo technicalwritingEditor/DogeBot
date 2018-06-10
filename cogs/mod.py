61b63cbfa1c15f281bbc19b1852a51de500fd1e2
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-10 11:33:58 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
-import random, asyncio, aiohttp
+import random, asyncio, aiohttp, datetime
 
 class mod():
     def __init__(self, bot):
         self.bot = bot
 
     async def on_member_join(self, user):
+        em = discord.Embed(description=f"Welcome **{user.mention}**!", color=0x9b9dff, timestamp = datetime.datetime.utcnow())
+        em.set_author(name=user, icon_url=user.avatar_url)
         x = await self.bot.db.welcome.find_one({"id": str(user.guild.id)})
         if not x:
             return
@@ -14,9 +16,11 @@ class mod():
         send_channel = self.bot.get_channel(channel)
         if not send_channel:
             return
-        await send_channel.send(f"Welcome {user.mention} to the guild!")
+        await send_channel.send(embed=em)
 
     async def on_member_remove(self, user):
+        em = discord.Embed(description=f"Goodbye **{user.name}**", color=0x9b9dff, timestamp = datetime.datetime.utcnow())
+        em.set_author(name=user, icon_url=user.avatar_url)
         x = await self.bot.db.leave.find_one({"id": str(user.guild.id)})
         if not x:
             return
@@ -24,7 +28,7 @@ class mod():
         send_channel = self.bot.get_channel(channel)
         if not send_channel:
             return
-        await send_channel.send(f"Goodbye **{user.name}**")        
+        await send_channel.send(embed=em)   
         
     @commands.command()
     async def welcome(self, ctx):
