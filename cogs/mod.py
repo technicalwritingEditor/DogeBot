209b5996d98e7e8f52f783bc13c5ffac7002d059
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-12 16:44:11 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -81,7 +81,7 @@ class mod():
                 x = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
             except asyncio.TimeoutError:
                  return await ctx.send("Request timed out. Please try again.")
-            await self.bot.db.leave.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel, "message": x.content}}, upsert=True)
+            await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel, "message": x.content}}, upsert=True)
             await ctx.send("Successfully turned on welcome messages for this guild.")            
         if sort == "off":
             await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": False, "message": None}}, upsert=True)
