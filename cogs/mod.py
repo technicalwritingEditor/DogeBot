6894b343de1ea8b146080b2637254b4ae73f161f
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-13 14:09:18 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -92,7 +92,14 @@ class mod():
             except asyncio.TimeoutError:
                  return await ctx.send("Request timed out. Please try again.")
             await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": channel, "message": x.content}}, upsert=True)
-            await ctx.send("Successfully turned on welcome messages for this guild.")            
+            await ctx.send("Successfully turned on message")   
+            await ctx.send("Do you want images? yes or no")
+            try:
+                y = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.channel and x.author == ctx.author, timeout=60.0)
+            except asyncio.TimeoutError:
+                 return await ctx.send("Request timed out. Please try again.")
+            if y == "yes":
+                await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"image": "on"}})
         if sort == "off":
             await self.bot.db.welcome.update_one({"id": str(ctx.guild.id)}, {"$set": {"channel": False, "message": None}}, upsert=True)
             await ctx.send("**I have turned off welcome messages**")
