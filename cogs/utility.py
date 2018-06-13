05ab8377d11ffef3f29248836e84f7296d314889
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-13 18:33:29 +0200

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -19,11 +19,13 @@ class utility():
         guild = ctx.guild
         time = str(guild.created_at.strftime("%b %m, %Y, %A, %I:%M %p"))
         embed=discord.Embed(description=f"**{guild.name}**\nOwner: **{guild.owner.mention}**\nMembers: **{len(guild.members)}**\nRoles: **{len(guild.members)}**\nVerification level: **{guild.verification_level}**\nCreated at: **{'%s' % time}**", color=0x06ff06)
+        embed.set_thumbnail(url=guild.icon_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def userinfo(self, ctx, user: discord.Member):
         embed=discord.Embed(description=f"{user.mention}\nId: **{user.id}**\nRoles: **{len(user.roles)}**\nStatus: **{user.status}**\nJoined at: **{user.joined_at}**", color=user.color)
+        embed.set_thumbnail(url=guild.icon_url)
         await ctx.send(embed=embed)
    
 def setup(bot):
