b98f3a3f990061098cdc15c68926e553558bde65
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-13 14:23:04 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -6,6 +6,10 @@ class mod():
     def __init__(self, bot):
         self.bot = bot
 
+    async def on_message(self, message):
+        if message.content == "hi":
+            await ctx.send("hello")        
+        
     async def on_member_join(self, user):
         x = await self.bot.db.welcome.find_one({"id": str(user.guild.id)})
         if not x:
