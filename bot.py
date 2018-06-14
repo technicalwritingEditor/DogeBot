8f18aea8293511a561998a8d6fc990ca5cb2e6a4
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-14 16:25:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def help(ctx):
     embed.add_field(name="Fun", value="`roast`, `face`, `lenny`, `tableflip`, `dog`", inline=False)
     embed.add_field(name="Economy", value="`create`, `work`, `bal`, `daily`", inline=False)
     embed.add_field(name="Images", value="`rip`, `achievement`, `avatar`", inline=False)
-    embed.add_field(name="Moderation", value="`welcome`, `leave`, `modlog`, `antiinvites`", inline=False)
+    embed.add_field(name="Moderation", value="`welcome`, `leave`, `modlog`, `antiinvites`, `kick`", inline=False)
     embed.add_field(name="Utility", value="`8ball`, `serverinfo`, `userinfo`", inline=False)
     embed.set_footer(text="I´m a very new bot and in early development, there will come A LOT more commands!")
     await ctx.send(embed=embed)
