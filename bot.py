3e210ebd506d3533654fcd4ef3fe72678276e869
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-15 18:00:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ bot.load_extension("cogs.utility")
 bot.load_extension("cogs.economy")
 bot.load_extension("cogs.mod")
 bot.load_extension("cogs.owner")
-bot.load_extension("cogs.premium")
 
 @bot.event
 async def on_ready():
