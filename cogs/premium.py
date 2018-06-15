68753e32bba3135ab5fb0fe9a8059fe1699b9760
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-15 17:52:04 +0200

    Add files via upload

---
+++ cogs/premium.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import random, asyncio, aiohttp, datetime
+
+class mod():
+    def __init__(self, bot):
+        self.bot = bot
+
+    def has_role_in_my_server(self):
+        def wrapper(self, ctx):
+            server = self.bot.get_guild(455305359645736971)
+            role = discord.utils.get(server.roles, name="premium")
+            user = discord.utils.get(server.members, id=ctx.author.id)
+            if not user or not role: return False
+            return role in user.roles
+        return commands.check(wrapper)
+
+    @commands.command()
+    @has_role_in_my_server("premium")
+    @commands.cooldown(1, 1800, commands.BucketType.user)
+    async def bigearn(self, ctx):
+        x = random.randint(10000, 100000)
+        user = await self.bot.db.configs.find_one( { "id": ctx.author.id } )
+        current = user['money']
+        self.bot.db.configs.update_one( { "id": ctx.author.id}, { "$set": { "money": current + x} })
+        await ctx.send(f"Your bigearn gave you {x}:dollar:!")      
+
+def setup(bot):
+    bot.add_cog(mod(bot))
