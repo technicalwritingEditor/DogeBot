9b25ab7534379a631119b4384cba581feff290d7
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-16 20:03:03 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -18,7 +18,7 @@ class info():
         RAM = psutil.virtual_memory()
         used = RAM.used >> 20
         percent = RAM.percent
-        embed=discord.Embed(description=f"**Information**\n\n__**Stats**__\nUptime: **%dd %dh %dm %ds**\nServers: **{len(self.bot.guilds)}**\nDiscord.py: **{discord.__version__}**\nMemory usage: {used} MB ({percent}%)%" (day, hour, minute, second),color=0x9b9dff)
+        embed=discord.Embed(description=f"**Information**\n\n__**Stats**__\nUptime: **%dd %dh %dm %ds**\nServers: **{len(self.bot.guilds)}**\nDiscord.py: **{discord.__version__}**\nMemory usage: {used} MB ({percent}%)" (day, hour, minute, second),color=0x9b9dff)
         await ctx.send(embed=embed)
 
     @commands.command()
