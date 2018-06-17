f8c18101e95c2e0f3a305653e6ef401631d484f0
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-17 13:37:55 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -23,7 +23,7 @@ class mod():
                 x = await self.bot.db.modlog.find_one({"id": str(message.guild.id)})
                 mod_channel = int(x['channel'])
                 xD = self.bot.get_channel(mod_channel)
-                embed = discord.Embed(description=f"📧 Invite deleted at {message.channel.mention}. Sent by {message.author.mention}!")
+                embed = discord.Embed(description=f"📧 Invite deleted at {message.channel.mention}. Sent by {message.author.mention}!", color=0x9b9dff)
                 await xD.send(embed=embed)
         if on_or_off == "off":
             pass
