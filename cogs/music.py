051c3c08c3208586b9b33bc3f553bb8c02884c78
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-17 08:50:42 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -3,7 +3,6 @@ import functools
 import logging
 import os
 import pathlib
-
 import discord
 import discord.ext.commands as commands
 import youtube_dl
@@ -271,7 +270,7 @@ class Music:
     async def nowplaying(self, ctx):
         if ctx.music_state.is_playing():
             song = ctx.music_state.current_song
-            await ctx.send(f'**Playing {ctx.song}. Volume at {song.volume * 100}% in {ctx.voice_client.channel}**')
+            await ctx.send(f'Playing {song.name}. Volume at {song.volume * 100}% in {ctx.voice_client.channel}')
         else:
             await ctx.send('Not playing.')
 
@@ -283,9 +282,6 @@ class Music:
     @commands.command(aliases=['summon', 'connect'])
     @commands.has_permissions(manage_guild=True)
     async def join(self, ctx, *, channel: discord.VoiceChannel = None):
-        """Summons the bot to a voice channel.
-        If no channel is given, summons it to your current voice channel.
-        """
         if channel is None and not ctx.author.voice:
             raise MusicError('**You are not in a voicechannel or told me where to join**')
 
@@ -325,7 +321,7 @@ class Music:
         else:
             # Schedule the song's download
             ctx.bot.loop.create_task(song.download(ctx.bot.loop))
-            await ctx.send(f'Queued {song} in position **#{ctx.music_state.playlist.qsize()}**')
+            await ctx.send(f'Queued **{song}** in position **#{ctx.music_state.playlist.qsize()}**')
 
         await ctx.message.remove_reaction('\N{HOURGLASS}', ctx.me)
         await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
@@ -357,7 +353,6 @@ class Music:
 
     @commands.command()
     async def volume(self, ctx, volume: int = None):
-        """Sets the volume of the player, scales from 0 to 100."""
         if volume < 0 or volume > 100:
             raise MusicError('**The volume level has to be between 0 and 100.**')
         ctx.music_state.volume = volume / 100
@@ -372,4 +367,5 @@ class Music:
         if not ctx.music_state.is_playing():
             raise MusicError('**Not playing anything to skip.**')
         else:
-            await ctx.music_state.skip.current_song()
+            song = ctx.music_state.current_song
+            await song.skip()
