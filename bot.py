b74cbf2972461bb19290b3eea566929bf7f78941
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-19 18:10:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def help(ctx):
     embed.add_field(name="Fun", value="`roast`, `face`, `lenny`, `tableflip`, `dog`", inline=False)
     embed.add_field(name="Economy", value="`openaccount`, `earn`, `bal`, `daily`", inline=False)
     embed.add_field(name="Images", value="`rip`, `achievement`, `avatar`", inline=False)
-    embed.add_field(name="Moderation", value="`welcome`, `leave`, `modlog`, `antiinvites`, `kick`, `ban`, `purge`, `warn`, `mute`, `unmute`", inline=False)
+    embed.add_field(name="Moderation", value="`welcome`, `leave`, `modlog`, `antiinvites`, `autorole`, `kick`, `ban`, `purge`, `warn`, `mute`, `unmute`", inline=False)
     embed.add_field(name="Music", value="`join`, `play`, `nowplaying`, `playlist`, `pause`, `stop`, `resume`, `clear`")
     embed.add_field(name="Giveaway", value="`start`", inline=False)
     embed.add_field(name="Utility", value="`8ball`, `serverinfo`, `userinfo`, `ping`", inline=False)
