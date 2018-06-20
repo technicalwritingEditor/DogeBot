e14b622e2a4009c9871ef8034b9f8ab3df8e9e11
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-20 10:47:15 +0200

    Update tags.py

--- cogs/tags.py
+++ cogs/tags.py
@@ -12,10 +12,10 @@ class tags():
             await ctx.send("Select a tag")
         if sort == "create":
             await self.bot.db.tags.update_one({"name": name}, {"$set": {"description": description} }, upsert=True )
-            await ctx.send(f"I have created a tag called {name}")
+            await ctx.message.add_reaction('✅')
         else:
             x = await self.bot.db.tags.find_one({"name": sort })
             await ctx.send(x['description'])
-
+            
 def setup(bot):
     bot.add_cog(tags(bot))
