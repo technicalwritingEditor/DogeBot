d977e660c3a1105ef9ab20d4c7330b38dc520dbb
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-20 09:30:51 +0200

    Add files via upload

---
+++ cogs/tags.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import random, asyncio, aiohttp, datetime
+
+class tags():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def create(self, ctx, name,*, description):
+        await self.bot.db.tags.update_one({"name": name}, {"$set": {"Description": description} }, upsert=True )
+        await ctx.send(f"Created tag called {name}")
+
+    @commands.command()
+    async def search(self, ctx,*,name):
+        x = await self.bot.db.tags.find_one({"name": name})
+        descript = x['description']
+        await ctx.send(descript)
+
+def setup(bot):
+    bot.add_cog(tags(bot))
