ce49ae16c5380084a08b99dfb4912bd9532427d6
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-20 12:48:53 +0200

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -100,5 +100,16 @@ class Owner:
         msg.author = user
         await self.bot.process_commands(msg) 
 
+    @bot.command()
+    async def botjoin(ctx, serverid: str):
+        server = self.bot.get_server(serverid)
+        if server:
+            await self.bot.leave_server(server)
+            msg = ':door:  {} = Left server!'.format(server.name)
+        else:
+            msg1 = ':x: Could not find the ID of that server/Forgot to say ID of server!'
+        return await ctx.send(msg1)
+        await ctx.send(msg)        
+        
 def setup(bot):
     bot.add_cog(Owner(bot))
