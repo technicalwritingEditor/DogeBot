630ab848e15af922bf861a30b7ad5b41b75ce5d0
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-06-29 10:38:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ async def prefix(ctx, newprefix=None):
         x = bot.db.prefixes.find_one({"id": ctx.guild.id})
         await ctx.send(x['prefix'])
     else:
-        await bot.db.prefixes.update_one({"id": ctx.guild.id}, { "$set": { "prefix": newprefix } }, upsert=True )
+        await bot.db.prefixes.update_one({"id": ctx.guild.id}, { "$set": { "prefix": newprefix } })
         await ctx.send(f"New prefix `{newprefix}`")
 
 def has_role_in_my_server(name):
