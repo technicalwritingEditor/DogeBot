7b4a39e75558714e048e77d9a56636aa56858b2e
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-07-03 09:48:10 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -17,14 +17,14 @@ class info():
         hour, minute = divmod(minute, 60)
         day, hour = divmod(hour, 24)
         RAM = psutil.virtual_memory()
-        used = RAM.used >> 20
+        used = RAM.used >> 30
         percent = RAM.percent
         embed=discord.Embed(title=f"{self.bot.user.name} stats", color=0x9b9dff)
         embed.set_thumbnail(url=self.bot.user.avatar_url)
         embed.add_field(name="Uptime", value="**%dd %dh %dm %ds**"% (day, hour, minute, second), inline=False)
         embed.add_field(name="Servers", value=f"Servers: **{len(self.bot.guilds)}**", inline=False)
         embed.add_field(name="Users", value=str(len(self.bot.users)))
-        embed.add_field(name="Memory used", value=f"{used}MB {percent}%", inline=False)
+        embed.add_field(name="Memory used", value=f"{used}GB {percent}%", inline=False)
 
         await ctx.send(embed=embed)
 
